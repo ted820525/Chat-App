dd1d1f2589f296e5d0d44df0abd25f64a5897f78
Darren <ted820525@gmail.com>
2020-03-09 08:42:57 +0800

    inital message

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 const path = require('path');
+const express = require('express');
 
 
-console.log(__dirname+"/../public");
-console.log(path.join(__dirname, "/../public"));
+// console.log(__dirname+"/../public");
+const publicPath = path.join(__dirname, "/../public");
+const port = process.env.PORT || 3000
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
+})
